5fdf1e4eddd253fed0bdda3b2b28618cbe4a03bf
s21014 <soccer0907>
2022-07-01 19:14:29 +0900

    Redisまで終了

--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/repository/CategoryRepositoryImpl.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/repository/CategoryRepositoryImpl.kt
@@ -1,64 +1,32 @@
 package jp.ac.it_college.std.s21014.news_manager.infrastructure.database.repository
 
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.CategoryDynamicSqlSupport.Category
+import jp.ac.it_college.std.s21014.news_manager.domain.model.CategoryModel
+import jp.ac.it_college.std.s21014.news_manager.domain.repository.CategoryRepository
 import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.CategoryMapper
 import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.deleteByPrimaryKey
 import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.insert
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.CategoryRecord
-import jp.ac.it_college.std.s21014.news_manager.domain.repository.CategoryRepository
-import org.mybatis.dynamic.sql.SqlBuilder.*
-import org.mybatis.dynamic.sql.render.RenderingStrategies
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.updateByPrimaryKeySelective
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.Category
 import org.springframework.stereotype.Repository
 
 @Repository
 class CategoryRepositoryImpl(
     private val categoryMapper: CategoryMapper
-): CategoryRepository {
-
-    private val columnList = listOf(
-        Category.id,
-        Category.name,
-    )
-
-    override fun findAll(): List<CategoryRecord> {
-        val selectStatement = select(columnList)
-            .from(Category)
-            .build()
-            .render(RenderingStrategies.MYBATIS3)
-        return categoryMapper.selectMany(selectStatement)
+) : CategoryRepository{
+    override fun register(category: CategoryModel) {
+        categoryMapper.insert(toCategory(category))
     }
 
-    override fun findById(id: Long): CategoryRecord? {
-        val selectStatement = select(columnList)
-            .from(Category)
-            .where(Category.id, isEqualTo(id))
-            .build()
-            .render(RenderingStrategies.MYBATIS3)
-        return categoryMapper.selectOne(selectStatement)
-    }
-    override fun findByName(name: String): CategoryRecord? {
-        val selectStatement = select(columnList)
-            .from(Category)
-            .where(Category.name, isEqualTo(name))
-            .build()
-            .render(RenderingStrategies.MYBATIS3)
-        return categoryMapper.selectOne(selectStatement)
-    }
-
-    override fun register(category: CategoryRecord) {
-        categoryMapper.insert(category)
+    override fun update(id: Long, name: String) {
+        categoryMapper.updateByPrimaryKeySelective(Category(id, name))
     }
 
     override fun delete(id: Long) {
         categoryMapper.deleteByPrimaryKey(id)
     }
 
-    override fun updateName(id: Long, name: String) {
-        val updateStatement = update(Category)
-            .set(Category.name).equalTo(name)
-            .where(Category.id, isEqualTo(id))
-            .build()
-            .render(RenderingStrategies.MYBATIS3)
-        categoryMapper.update(updateStatement)
+    private fun toCategory(model: CategoryModel): Category {
+        return Category(model.id, model.name)
     }
+
 }
--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/repository/UsersRepositoryImpl.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/repository/UsersRepositoryImpl.kt
@@ -1,39 +1,34 @@
 package jp.ac.it_college.std.s21014.news_manager.infrastructure.database.repository
 
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.UsersRecord
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.UsersDynamicSqlSupport.Users
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.UsersMapper
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.insert
 import jp.ac.it_college.std.s21014.news_manager.domain.repository.UserRepository
-import org.mybatis.dynamic.sql.SqlBuilder.select
-import org.mybatis.dynamic.sql.SqlTable
-import org.mybatis.dynamic.sql.render.RenderingStrategies
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.UsersDynamicSqlSupport
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.UsersDynamicSqlSupport.username
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.UsersMapper
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.selectOne
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.Users
 import org.mybatis.dynamic.sql.util.kotlin.elements.isEqualTo
 import org.springframework.stereotype.Repository
 
 @Repository
 class UserRepositoryImpl(
-    private val usersMapper: UsersMapper
+    private val mapper: UsersMapper,
 ) : UserRepository {
-
-    private val columnList = listOf(
-        Users.id,
-        Users.username,
-        Users.password,
-        Users.viewName,
-        Users.roleType
-    )
-
-    override fun find(username: String): UsersRecord? {
-        val selectStatement = select(columnList)
-            .from(SqlTable.of("users"))
-            .where(Users.username, isEqualTo(username))
-            .build()
-            .render(RenderingStrategies.MYBATIS3)
-        return usersMapper.selectOne(selectStatement)
+    override fun find(email: String): Users? {
+        val record = mapper.selectOne {
+            where {
+                UsersDynamicSqlSupport.users.username isEqualTo email
+            }
+        }
+        return record?.let { toModel(it) }
     }
 
-    override fun register(user: UsersRecord) {
-        usersMapper.insert(user)
+    private fun toModel(record: Users) : Users {
+        return Users(
+            record.id!!,
+            record.username!!,
+            record.password!!,
+            record.viewName!!,
+            record.roleType!!
+        )
     }
 }

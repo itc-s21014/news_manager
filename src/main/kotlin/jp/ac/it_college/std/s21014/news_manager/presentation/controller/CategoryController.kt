5dfb036d50ce15a0426624b29e6679221ee2c304
s21014 <soccer0907>
2022-07-01 19:17:11 +0900

    Redisまで終了

--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/presentation/controller/CategoryController.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/presentation/controller/CategoryController.kt
@@ -1,28 +1,39 @@
 package jp.ac.it_college.std.s21014.news_manager.presentation.controller
 
-import jp.ac.it_college.std.s21014.news_manager.application.service.CategoryService
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.CategoryRecord
+import jp.ac.it_college.std.s21014.news_manager.application.service.AdminCategoryService
+import jp.ac.it_college.std.s21014.news_manager.domain.model.CategoryModel
 import jp.ac.it_college.std.s21014.news_manager.presentation.form.RegisterCategoryRequest
 import jp.ac.it_college.std.s21014.news_manager.presentation.form.UpdateCategoryRequest
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
-@RequestMapping("/category")
-data class CategoryController(
-    private val categoryService: CategoryService
+@RequestMapping("category")
+@CrossOrigin(origins = ["http://localhost8081/"], allowCredentials = "true")
+class CategoryController(
+    private val adminCategoryService: AdminCategoryService
 ) {
     @PostMapping("/register")
     fun register(@RequestBody request: RegisterCategoryRequest) {
-        categoryService.register(CategoryRecord(0, request.name))
-    }
-
-    @DeleteMapping("/delete/{id}")
-    fun delete(@PathVariable("id") id: Long) {
-        categoryService.delete(id)
+        adminCategoryService.register(
+            CategoryModel(0, request.name)
+        )
     }
 
     @PostMapping("/update")
     fun update(@RequestBody request: UpdateCategoryRequest) {
-        categoryService.update(request.id, request.name)
+        println("登録完了しました")
+        adminCategoryService.update(CategoryModel(request.id, request.name))
+    }
+
+    @DeleteMapping("/delete/{category_id}")
+    fun delete(@PathVariable("category_id") categoryId: Long) {
+        adminCategoryService.delete(categoryId)
     }
 }

d09931da1deee10a9ce7ffe1bad5ecd4149a67d3
s21014 <soccer0907>
2022-07-01 19:16:29 +0900

    SpringSecurityの認証処理に関するインタフェースを実装まで

--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/presentation/controller/NewsController.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/presentation/controller/NewsController.kt
@@ -1,54 +1,33 @@
 package jp.ac.it_college.std.s21014.news_manager.presentation.controller
 
-import jp.ac.it_college.std.s21014.news_manager.application.service.NewsManagerUserDetails
 import jp.ac.it_college.std.s21014.news_manager.application.service.NewsService
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.BundledNewsRecord
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.NewsRecord
-import jp.ac.it_college.std.s21014.news_manager.domain.enum.RoleType
-import jp.ac.it_college.std.s21014.news_manager.domain.repository.CategoryRepository
-import jp.ac.it_college.std.s21014.news_manager.presentation.form.RegisterNewsRequest
-import org.springframework.security.core.Authentication
-import org.springframework.web.bind.annotation.*
-import java.time.LocalDateTime
+import jp.ac.it_college.std.s21014.news_manager.presentation.form.GetNewsDetailResponse
+import jp.ac.it_college.std.s21014.news_manager.presentation.form.GetNewsListResponse
+import jp.ac.it_college.std.s21014.news_manager.presentation.form.NewsInfo
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("news")
+@CrossOrigin(origins = ["http://localhost:8081"], allowCredentials = "true")
+
 class NewsController(
-    private val newsService: NewsService,
-    private val categoryRepository: CategoryRepository
-) {
-
-    @RequestMapping("/news")
-    fun getNews(
-        @RequestParam("page", name = "page", required = false, defaultValue = "1") page: Long,
-        authentication: Authentication?
-    ): List<BundledNewsRecord> {
-        val isAdmin =
-            authentication?.authorities?.map { RoleType.valueOf(it.authority) }?.contains(RoleType.ADMIN) ?: false
-        return newsService.getList(isAdmin, page)
+    private val newsService: NewsService
+){
+    @GetMapping("/list")
+    fun getList(): GetNewsListResponse{
+        val newsList = newsService.getList().map {
+            NewsInfo(it)
+        }
+        return GetNewsListResponse(newsList)
     }
 
-    @GetMapping("/news/{id}")
-    fun getNewsDetail(@PathVariable id: Long, authentication: Authentication?): BundledNewsRecord {
-        val isAdmin =
-            authentication?.authorities?.map { RoleType.valueOf(it.authority) }?.contains(RoleType.ADMIN) ?: false
-        return newsService.getById(id, isAdmin)
-    }
-
-    @PostMapping("/post")
-    fun register(@RequestBody request: RegisterNewsRequest, authentication: Authentication) {
-        categoryRepository.findById(request.categoryId)
-            ?: throw IllegalArgumentException("存在しないカテゴリID: ${request.categoryId}")
-        val principal = authentication.principal as NewsManagerUserDetails
-        newsService.register(
-            NewsRecord(
-                0,
-                request.title,
-                request.categoryId,
-                request.publishAt,
-                LocalDateTime.now(),
-                principal.id,
-                request.body
-            )
-        )
+    @GetMapping("/detail/{news_id}")
+    fun getDetail(@PathVariable("news_id") newsId: Long) : GetNewsDetailResponse {
+        val news = newsService.getDetail(newsId)
+        return GetNewsDetailResponse(news)
     }
 }

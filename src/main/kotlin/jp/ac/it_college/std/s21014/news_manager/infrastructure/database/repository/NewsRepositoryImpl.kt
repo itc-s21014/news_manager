b74a8e15cc516009be4e14b9e709a3f5dfd0c779
s21014 <soccer0907>
2022-07-01 19:13:50 +0900

    SpringSecurityの認証処理に関するインタフェースを実装まで

--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/repository/NewsRepositoryImpl.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/repository/NewsRepositoryImpl.kt
@@ -1,25 +1,51 @@
 package jp.ac.it_college.std.s21014.news_manager.infrastructure.database.repository
 
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.*
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.BundledNewsRecord
-import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.NewsRecord
+import jp.ac.it_college.std.s21014.news_manager.domain.model.CategoryModel
+import jp.ac.it_college.std.s21014.news_manager.domain.model.NewsModel
+import jp.ac.it_college.std.s21014.news_manager.domain.model.NewsWithCategoryModel
 import jp.ac.it_college.std.s21014.news_manager.domain.repository.NewsRepository
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.mapper.*
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.NewsWithCategoryRecord
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.News
 import org.springframework.stereotype.Repository
 
 @Repository
 class NewsRepositoryImpl(
-    private val bundledNewsMapper: BundledNewsMapper,
+    private val newsWithCategoryMapper: NewsWithCategoryMapper,
     private val newsMapper: NewsMapper
 ) : NewsRepository {
-    override fun findAll(includeUnpublished: Boolean, page: Long): List<BundledNewsRecord> {
-        return bundledNewsMapper.select(includeUnpublished, page)
+    override fun findAllWithCategory(): List<NewsWithCategoryModel> {
+        return newsWithCategoryMapper.select {  }.map { toModel(it) }
     }
 
-    override fun findById(id: Long, includeUnpublished: Boolean): BundledNewsRecord {
-        return bundledNewsMapper.selectByPrimaryKey(id, includeUnpublished)
+    override fun findWithCategory(id: Long): NewsWithCategoryModel? {
+        return newsWithCategoryMapper.selectByPrimaryKey(id)?.let { toModel(it) }
     }
 
-    override fun register(news: NewsRecord) {
-        newsMapper.insert(news)
+    override fun register(news: News) {
+        newsMapper.insert(toCategory(news))
+    }
+
+    private fun toModel(record: NewsWithCategoryRecord): NewsWithCategoryModel {
+        val news = NewsModel(
+            record.id!!,
+            record.title!!,
+            record.categoryId!!,
+            record.publishAt!!,
+            record.createAt!!,
+            record.userId!!,
+            record.body!!
+        )
+        val category = record.id?.let {
+            CategoryModel(
+                record.id!!,
+                record.name!!
+            )
+        }
+        return NewsWithCategoryModel(news, category)
+    }
+
+    private fun toCategory(model: News) : News {
+        return News(model.id, model.title, model.categoryId, model.publishAt, model.createAt, model.userId, model.body)
     }
 }

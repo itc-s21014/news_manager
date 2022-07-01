91b32f574dd9a83b4860778ebe5fa89fad9a7c95
s21014 <soccer0907>
2022-07-01 19:17:37 +0900

    Redisまで終了

--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/presentation/form/NewsFrom.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/presentation/form/NewsFrom.kt
@@ -1,19 +1,47 @@
 package jp.ac.it_college.std.s21014.news_manager.presentation.form
 
-import jp.ac.it_college.std.s21014.news_manager.domain.enum.RoleType
+import jp.ac.it_college.std.s21014.news_manager.domain.model.NewsWithCategoryModel
+import jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record.Category
+import java.sql.Timestamp
 import java.time.LocalDateTime
 
-data class RegisterNewsRequest(
+data class GetNewsListResponse(val newsList: List<NewsInfo>)
+
+data class NewsInfo(
     val title: String,
     val body: String,
     val categoryId: Long,
-    val publishAt: LocalDateTime = LocalDateTime.now()
-)
-data class RegisterUserRequest(
-    val username: String,
-    var password: String,
-    val viewName: String = username,
-    val roleType: RoleType = RoleType.USER
+    val publishAt: Timestamp
+){
+    constructor(model: NewsWithCategoryModel) : this(
+        model.news.title!!, model.news.body!!, model.news.categoryId!!, model.news.publishAt!!
+    )
+}
+
+data class GetNewsDetailResponse(
+    val id: Long,
+    val title: String,
+    val userId: Long
+){
+    constructor(model: NewsWithCategoryModel) : this(
+        model.news.id!!,
+        model.news.title!!,
+        model.news.userId!!
+    )
+}
+
+data class CategoryInfo(
+    val id: Long,
+    val name: String
+) {
+    constructor(category: Category) : this(category.id!!, category.name!!)
+}
+
+data class RegisterNewsRequest(
+    val categoryId: Long,
+    val title: String,
+    val body: String,
+    val publishAt: LocalDateTime
 )
 
 data class RegisterCategoryRequest(

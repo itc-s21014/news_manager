6c9afe8834c7971503e1e46d8c0e2c28f5586df3
s21014 <soccer0907>
2022-07-01 19:04:38 +0900

    application.ymlでデータベースとJacksonの設定

---
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/record/NewsWithCategoryRecord.kt
@@ -0,0 +1,14 @@
+package jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record
+
+import java.sql.Timestamp
+
+data class NewsWithCategoryRecord(
+    val id: Long? = null,
+    val title: String? = null,
+    val categoryId: Long? = null,
+    val createAt: Timestamp? = null,
+    val publishAt: Timestamp? = null,
+    val userId: Long? = null,
+    val body: String? = null,
+    val name: String? = null,
+)

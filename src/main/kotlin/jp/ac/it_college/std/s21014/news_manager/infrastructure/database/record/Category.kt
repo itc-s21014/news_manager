5ceae35031a151d453ce00aecaf9f4a92b58924c
s21014 <soccer0907>
2022-07-01 19:05:25 +0900

    application.ymlでデータベースとJacksonの設定

--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/record/Category.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/record/Category.kt
@@ -1,6 +1,9 @@
+/*
+ * Auto-generated file. Created by MyBatis Generator
+ */
 package jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record
 
-data class CategoryRecord(
+data class Category(
     var id: Long? = null,
     var name: String? = null
 )
--- src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/record/Users.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21014/news_manager/infrastructure/database/record/Users.kt
@@ -1,8 +1,11 @@
+/*
+ * Auto-generated file. Created by MyBatis Generator
+ */
 package jp.ac.it_college.std.s21014.news_manager.infrastructure.database.record
 
 import jp.ac.it_college.std.s21014.news_manager.domain.enum.RoleType
 
-data class UsersRecord(
+data class Users(
     var id: Long? = null,
     var username: String? = null,
     var password: String? = null,
